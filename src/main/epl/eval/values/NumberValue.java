7bfba8f97e53c319eedfa7b09159dda6d8056a42
ControlFlow Graph <controlflowgraph@gmail.com>
2021-11-22 21:53:19 +0100

    add arithmetic operations to NumberValue

--- src/main/epl/eval/values/NumberValue.java
+++ src/main/epl/eval/values/NumberValue.java
@@ -1,10 +1,67 @@
 package epl.eval.values;
 
-public record NumberValue(double number) implements Value
+import epl.eval.operations.arithmetic.*;
+
+public record NumberValue(double number) implements Value, Addition, Subtraction, Multiplication, Division, Modulo
 {
     @Override
     public String toString()
     {
         return Double.toString(this.number);
+    }
+
+    @Override
+    public Value add(Value value)
+    {
+        if(value instanceof NumberValue n)
+        {
+            return new NumberValue(this.number + n.number);
+        }
+        return unsupported(value);
+    }
+
+    @Override
+    public Value sub(Value value)
+    {
+        if(value instanceof NumberValue n)
+        {
+            return new NumberValue(this.number - n.number);
+        }
+        return unsupported(value);
+    }
+
+    @Override
+    public Value mul(Value value)
+    {
+        if(value instanceof NumberValue n)
+        {
+            return new NumberValue(this.number * n.number);
+        }
+        return unsupported(value);
+    }
+
+    @Override
+    public Value div(Value value)
+    {
+        if(value instanceof NumberValue n)
+        {
+            return new NumberValue(this.number / n.number);
+        }
+        return unsupported(value);
+    }
+
+    @Override
+    public Value mod(Value value)
+    {
+        if(value instanceof NumberValue n)
+        {
+            return new NumberValue(this.number % n.number);
+        }
+        return unsupported(value);
+    }
+
+    private Value unsupported(Value value)
+    {
+        throw new RuntimeException("Unsupported operation of types " + this.getClass().getName() + " and " + value.getClass().getName());
     }
 }
